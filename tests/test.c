967ff97114e1bf76962fea07e69f7cbe72d182dd
josephjoel3099 <josephjoel3099@gmail.com>
2026-01-16 20:32:12 +0000

    fix: update test file to use device pointer in function calls for LIS3MDL

--- tests/test.c
+++ tests/test.c
@@ -3,25 +3,28 @@
 
 int main(void)
 {
+    lis3mdl_t sensor;
+    lis3mdl_init(&sensor, 0x1E);
+
     uint8_t fs;
     uint8_t odr;
     int16_t raw_x;
     double x;
 
-    get_full_scale_config(&fs);
+    get_full_scale_config(&sensor, &fs);
     printf("Full scale: ±%d gauss\n", fs);
 
-    set_odr(20);
+    set_odr(&sensor, 20);
 
-    get_odr(&odr);
+    get_odr(&sensor, &odr);
     printf("ODR: %d Hz\n", odr);
 
-    enable_interrupt(true);
+    enable_interrupt(&sensor, true);
 
-    read_raw_axis_data(X_AXIS, &raw_x);
+    read_raw_axis_data(&sensor, X_AXIS, &raw_x);
     printf("X axis (raw): %d\n", raw_x);
 
-    read_axis_data(X_AXIS, &x);
+    read_axis_data(&sensor, X_AXIS, &x);
     printf("X axis (gauss): %f uT\n", x);
 
     return 0;
